5b52cb821be9db8ed2a3ddf2f6f5e951efbd4ea5
PedroAugustoAO <pedroaugao@gmail.com>
2026-04-24 09:42:29 -0300

    Update app.py

--- Jogo do Bicho/Jogo do Bicho/app.py
+++ Jogo do Bicho/Jogo do Bicho/app.py
@@ -4,7 +4,7 @@ def get_db_connection():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="100907",
+        password="1234",
         database="banco_dados"
     )
 
